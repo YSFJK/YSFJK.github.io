f7616db68ba528a45baaf9d3de0e58a3a182482f
YSFJK <klnofrontier@gmail.com>
2021-04-04 00:41:57 +0900

    11th

---
+++ achievement.js
@@ -0,0 +1,3 @@
+$(function(){
+	$('.main_page').addClass('fadein');
+});
--- index.js
+++ index.js
@@ -4,9 +4,20 @@ $(function(){
 			var elemPos = $(this).offset().top;
 			var scroll = $(window).scrollTop();
 			var windowHeight = $(window).height();
-			if (scroll > elemPos - windowHeight + 200){
-				$(this).addClass('scrollin');
+			if (scroll > elemPos - windowHeight ){
+				$(this).removeClass('fadein_out');
+				$(this).addClass('fadein_in');
+			}
+			else{
+				$(this).removeClass('fadein_in');
+				$(this).addClass('fadein_out');
 			}
 		});
 	});
+});
+$(document).on('click','.achievement', function() {
+	$('.main_page').addClass('fadeout');
+	setTimeout(function(){
+		window.location.href = "ahievement.html";
+	},1000);
 });
